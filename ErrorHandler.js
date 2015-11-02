12455a5ed0eb23069262e488f4a7de022a5785df
bababa <me@mail.me>
2015-11-02 15:36:19 +0200

    add error handler

---
+++ ErrorHandler.js
@@ -0,0 +1,15 @@
+
+    var getErrorString = function(Id,Exception)
+    {
+        if (Exception != null)
+        {
+            //writeToLog
+        }
+        else
+        {
+            return "Error Tester";
+
+        }
+    }
+
+module.exports.getErrorString = getErrorString;
